a19bbb1fccb340794a9f64c7755d384357a32920
drmagu <willywouters.phd@gmail.com>
2023-05-26 17:01:11 -0500

    updated caesar

--- 4 Some Ciphers/caesar_class.py
+++ 4 Some Ciphers/caesar_class.py
@@ -1,53 +1,49 @@
 class Caesar:
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    def __init__(self, shift=13):
-        self.shift = shift
-        # check shift
-        if not abs(shift) < len(self.alphabet) + 1:
-            print(f"invalid shift parameter") 
-            exit()
+    """Implements a Caesar cipher with customizable shift."""
+     
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
-    def _crypt(self, message: str, type: str='e') -> str:
+    def __init__(self, shift: int = 13):
+        """Initializes Caesar with a provided shift or default shift 13."""
+        if not abs(shift) < len(self.ALPHABET) + 1:
+            raise ValueError("Invalid shift.")
+        self.shift = shift
+
+    def _encrypt_decrypt(self, message: str, encrypt: bool = True) -> str:
+        """Encrypts or decrypts a message depending on the 'encrypt' parameter."""
+        cleaned_message = self._clean_message(message)
         cipher = []
-        message = self.clean(message)
-        for letter in message:
-            index = self.alphabet.index(letter)
-            if type == 'e':
-                new_index = (index + self.shift) % len(self.alphabet)
-            elif type == 'd':
-                new_index = (index - self.shift) % len(self.alphabet)
-            cipher.append(self.alphabet[new_index])
+        for letter in cleaned_message:
+            index = self.ALPHABET.index(letter)
+            if encrypt:
+                new_index = (index + self.shift) % len(self.ALPHABET)
+            else:
+                new_index = (index - self.shift) % len(self.ALPHABET)
+            cipher.append(self.ALPHABET[new_index])
         return ''.join(cipher)
     
-    def clean(self, message):
-        message = message.lower() 
-        message_list = list(message)
-        for letter in message:
-            if not letter in self.alphabet:
-                message_list.remove(letter)
-        return ''.join(message_list)
+    def _clean_message(self, message: str) -> str:
+        """Cleans a message by converting to lower case and removing non-alphabet characters."""
+        return ''.join(char for char in message.lower() if char in self.ALPHABET)
     
-    def encrypt(self, message):
-        return self._crypt(message)
+    def encrypt(self, message: str) -> str:
+        """Encrypts a message."""
+        return self._encrypt_decrypt(message, encrypt=True)
     
-    def decrypt(self, cipher):
-        return self._crypt(cipher, 'd')
+    def decrypt(self, cipher: str) -> str:
+        """Decrypts a cipher."""
+        return self._encrypt_decrypt(cipher, encrypt=False)
 
 if __name__ == "__main__":
-    ## some tests
     csr = Caesar(-13)
     message = "attackatdawn"
     print(f"message: {message}")
     print(f"cipher: {csr.encrypt(message)}")
     print(f"decrypted: {csr.decrypt(csr.encrypt(message))}")
-    print()
 
     csr = Caesar(7)
     message = "the Dog ate MY HOMEWORK!"
     print(f"message: {message}")
-    print(f"cleaned: {csr.clean(message)}")
+    print(f"cleaned: {csr._clean_message(message)}")
     print(f"cipher: {csr.encrypt(message)}")
     print(f"decrypted: {csr.decrypt(csr.encrypt(message))}")
-    print()
-
-
