c8d8302ee4720fb5eb00d039369980fbec49cff0
drmagu <willywouters.phd@gmail.com>
2023-05-27 08:21:06 -0500

    optimized code

--- 5 Graphs in Python/graph_class.py
+++ 5 Graphs in Python/graph_class.py
@@ -1,18 +1,30 @@
 
-from copy import copy
+from typing import Optional, List, Any
 
 class Graph:
+    """
+    This class represents a simple undirected graph using an adjacency list.
+    """
     def __init__(self):
+        """
+        Constructor of the Graph class.
+        """
         self.graph = dict()
         self.nodes = []
-        
-    def add_node(self, node) -> None:
+
+    def add_node(self, node: str) -> None:
+        """
+        Method to add a node to the graph.
+        """
         if node not in self.nodes:
             self.nodes.append(node)
             self.graph[node] = []
         return
 
     def add_edge(self, edge: tuple) -> None:
+        """
+        Method to add an edge to the graph. Edge is a tuple of two nodes.
+        """
         u, v = edge[0], edge[1]
         self.add_node(u)
         self.add_node(v)
@@ -20,22 +32,23 @@ class Graph:
         self.graph[v].append(u)
         return
 
-    def show_edges(self) -> list:
-        edges = []
-        for node in self.graph:
-            for neighbor in self.graph[node]:
-                edges.append((node, neighbor))
-        return edges
+    def get_edges(self) -> List[tuple]:
+        """
+        Method to get all edges of the graph.
+        """
+        return [(node, neighbor) for node in self.graph for neighbor in self.graph[node]]
 
-
-    def find_path(self, start, end, path=None) -> list:
+    def find_path(self, start: str, end: str, path: Optional[List[str]] = None) -> Optional[List[str]]:
+        """
+        Recursive method to find a path from 'start' to 'end'.
+        """
         if path is None:
             path = []
-        path.append(start)   
+        path.append(start)
+
         if start == end:
             return path
-        neighbors = self.graph[start]
-        for node in neighbors:
+        for node in self.graph[start]:
             if node not in path:
                 newpath = self.find_path(node, end, path)
                 if newpath:
@@ -43,6 +56,9 @@ class Graph:
         return None
     
     def find_paths(self, start, end, path=None) -> list:
+        """
+        Method to find all paths from 'start' to 'end'.
+        """
         if path is None:
             path = []
         path.append(start)
@@ -56,42 +72,41 @@ class Graph:
                     paths.append(newpath)
         return paths
 
-    def find_all_paths(self) -> list:
-        paths = []
-        for node in self.nodes:
-            for end in self.nodes:
-                if end != node:
-                    path = self.find_path(node, end)
-                    paths.append(path)
-        return paths
+    def find_all_paths(self) -> List[List[Any]]:
+        """
+        Finds all possible paths between each pair of nodes in the graph.
+        
+        Returns:
+            List of lists, where each list represents a path in the graph.
+        """
+        all_paths = []
+        for start_node in self.nodes:
+            for end_node in self.nodes:
+                if start_node != end_node:
+                    path = self.find_path(start_node, end_node)
+                    if path is not None:
+                        all_paths.append(path)
+        return all_paths
 
-    def shortest_path(self) -> list:
-        paths = self.find_all_paths()
-        clean_paths = []
-        if None in paths:
-            for path in paths:
-                if path is not None:
-                    paths.remove(None)
-                    clean_paths.append(path)
-        else:
-            clean_paths = paths
-        return min(clean_paths, key=len)
-    
-    def longest_path(self) -> list:
-        paths = self.find_all_paths()
-        clean_paths = []
-        if None in paths:
-            for path in paths:
-                if path is not None:
-                    paths.remove(None)
-                    clean_paths.append(path)
-        else:
-            clean_paths = paths
-        return max(clean_paths, key=len)
-    
-    def hamiltonian(self) -> bool:
+    def shortest_path(self) -> Optional[List[str]]:
+        """
+        Method to find the shortest path from in the graph.
+        """
+        all_paths = self.find_all_paths()
+        return min(all_paths, key=len, default=None)
+
+    def longest_path(self) -> Optional[List[str]]:
+        """
+        Method to find the longest path from 'start' to 'end'.
+        """
+        all_paths = self.find_all_paths()
+        return max(all_paths, key=len, default=None)
+
+    def is_hamiltonian(self) -> bool:
+        """
+        Method to check if the graph is Hamiltonian. 
+        It checks if the longest path includes all the nodes.
+        """
         if len(self.longest_path()) == len(self.nodes):
             return True
         return False
-    
-
